4a70da8ec607e0da07f45c8c74a098597411d75f
Michael <AstroMike101@gmail.com>
2021-12-23 16:07:36 -0800

    big rework, got the objects working, currently working to make the "cards" to hold the objects

--- main.js
+++ main.js
@@ -17,34 +17,70 @@ function addBookToLibrary() {
 
 // }
 
-let mainPage = document.querySelector('.books');
-let bookBtn = document.querySelector('.addBookBtn');
-let addBookPage = document.querySelector('.addBookPage');
+let mainPage = document.querySelector('.books'); //main page of website
+let addBookPage = document.querySelector('.addBookPage'); //the div that has the form
+let bookBtn = document.querySelector('.addBookBtn');  //add book button 
+let submitBtn = document.querySelector('#submitBox'); //the submit box in the form 
+let mainform = document.querySelector('#mainForm'); //the <form> element
 
+
+
+//values for the input forms
+let bookName = document.querySelector('#bookName').value; //name of book form value
+let authorName = document.querySelector('#authorName').value; // name of author form
+let pages = document.querySelector('#pages').value; // # of pages form 
+let readOrNot = document.querySelector('#checkbox').value; //checkbox for if read or not 
+
+
+//function that creates a new book whenever submit is clicked
+// submitBtn.addEventListener('click', () => { 
+//   console.log(authorName);
+//  let book2= new Book((authorName,bookName,pages,readOrNot));
+//  console.log(book2);
+
+// })
+
+//prevents the submit button from refreshing everything
+let formvalues = document.querySelectorAll('.formBoxes');
+//transform the data inside the forms into an object
+mainform.addEventListener('submit', (e) => { 
+e.preventDefault();
+let newBook = {
+  bookName:document.querySelector('#bookName').value, //name of book form value
+  authorName:document.querySelector('#authorName').value, // name of author form
+  pages:document.querySelector('#pages').value, // # of pages form 
+  readOrNot:document.querySelector('#checkbox').value //checkbox for if read
+
+}
+myLibrary.push(newBook);
+formvalues.forEach(formvalues =>  formvalues.value = '');
+console.log(myLibrary);
+newCard();
+myLibrary = [];
+})
 
 
 
 
 
 bookBtn.addEventListener('click', () => { 
-    createAddBookPage();
-
+    showAddBookPage();
 })
 
-
-
-function createAddBookPage () { 
-}
+submitBtn.addEventListener('click', () => { 
+    hideBookPage();
+})
 
 
 
 
 function showAddBookPage () { 
-
+addBookPage.style.display = "flex";
 }
 
 
 function hideBookPage () { 
+    addBookPage.style.display = "none";
 
 }
 
@@ -53,13 +89,24 @@ function hideBookPage () {
 function newCard() { 
     let newdiv  = document.createElement('div');
     newdiv.className+='newBook';
-    let info = document.createElement('p');
-    info.textContent = "TItle";
-    
-    newdiv.appendChild(info);
+    let bookName = document.createElement('p');
+    bookName.textContent = myLibrary[0].bookName;
+    let authorName = document.createElement('p');
+    authorName.textContent = myLibrary[0].authorName;
+    let pages = document.createElement('p');
+    pages.textContent = myLibrary[0].pages;
+    let readOrNot = document.createElement('p');
+    readOrNot.textContent = myLibrary[0].readOrNot;
+    newdiv.appendChild(bookName);
+    newdiv.appendChild(authorName);
+    newdiv.appendChild(pages);
+    newdiv.appendChild(readOrNot);
     mainPage.appendChild(newdiv);
+
     
 }
+
+
 
     
 
